a0b12804df6ff74cd955bb7eff08c97afdf6df5c
Sf-Shohag71 <shakhfarid71@gmail.com>
2023-05-31 19:42:16 +0600

    change the titile

--- src/Pages/Home/Gellary/Gellary.jsx
+++ src/Pages/Home/Gellary/Gellary.jsx
@@ -2,7 +2,7 @@ import { MdToys } from "react-icons/md";
 const Gellary = () => {
   return (
     <div className="container mx-auto px-5 py-2 lg:px-32 lg:pt-16">
-        <h3 className="text-5xl text-center font-bold mb-8" data-aos="fade-right">Latest Toys</h3>
+        <h3 className="text-5xl text-center font-bold mb-8" data-aos="fade-right">Latest Medicines</h3>
         <div className="divider text-6xl text-orange-500"><MdToys></MdToys></div>
       <div className="-m-1 flex flex-wrap md:-m-2">
         <div className="flex w-1/3 flex-wrap">
